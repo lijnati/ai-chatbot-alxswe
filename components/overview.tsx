70a1b6e59ecd82318b57a55fd14efa8e634b83a8
lijnati <lijnati3@gmail.com>
2024-12-03 10:06:05 +0000

    changes on:overview.tsx

--- components/overview.tsx
+++ components/overview.tsx
@@ -6,7 +6,7 @@ export const Overview = () => {
         <div className="w-1/2 text-center p-4">
           <h2 className="text-xl font-semibold">AI Chatbot - ASTUCSEC Project</h2>
           <p className="text-white-600">Developed by <span className='font-italic'> <Link href='https://github.com/lijnati'>Yegetaneh Firew</Link></span></p>
-          <p className="text-gray-600 text-underline"><Link href='https://github.com/lijnati/ai-chatbot-astucsec-v1'>Star on Github⭐</Link></p>
+          <p className="text-gray-600 underline"><Link href='https://github.com/lijnati/ai-chatbot-astucsec-v1'>Star on Github⭐</Link></p>
         </div>
       </div>
 
